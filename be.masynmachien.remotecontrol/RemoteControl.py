98783567ac1cfd1c628aed3e84e2798e64e90bbf
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2026-07-28 20:49:34 +0200

    joystick: increase dead zone, await 3 processes

--- be.masynmachien.remotecontrol/RemoteControl.py
+++ be.masynmachien.remotecontrol/RemoteControl.py
@@ -184,22 +184,22 @@ class RemoteControl(Activity):
 
     def refresh_joystick(self, timer):
         raw_x, raw_y = joystick.read_raw()
-        center = 100
-        border = 100
+        dead_center = 400
+        dead_border = 200
         
         # Map de analoge waarden naar schermcoördinaten voor het bolletje
         if self.circ_area:
             # x_pos = lv.map(raw_x, 0, 4095, -int(JOYSTICK_RECTANGLE_WIDTH/2), int(JOYSTICK_RECTANGLE_WIDTH/2))
             # y_pos = lv.map(raw_y, 4095, 0, -int(JOYSTICK_RECTANGLE_HEIGHT/2), int(JOYSTICK_RECTANGLE_HEIGHT/2))
-            x_pos = map_joystick(raw_x, 0, 4095, -int(JOYSTICK_RECTANGLE_WIDTH/2), int(JOYSTICK_RECTANGLE_WIDTH/2), center, border)
-            y_pos = map_joystick(raw_y, 4095, 0, -int(JOYSTICK_RECTANGLE_HEIGHT/2), int(JOYSTICK_RECTANGLE_HEIGHT/2), center, border)
+            x_pos = map_joystick(raw_x, 0, 4095, -int(JOYSTICK_RECTANGLE_WIDTH/2), int(JOYSTICK_RECTANGLE_WIDTH/2), dead_center, dead_border)
+            y_pos = map_joystick(raw_y, 4095, 0, -int(JOYSTICK_RECTANGLE_HEIGHT/2), int(JOYSTICK_RECTANGLE_HEIGHT/2), dead_center, dead_border)
             self.circ_area.set_pos(x_pos + int(JOYSTICK_CIRCLE_RADIUS/2), y_pos + int(JOYSTICK_CIRCLE_RADIUS/2))
 
         # Map de ADC waarden naar het bereik -180 tot 180 voor de WebSocket
         # self.joy_x = lv.map(raw_x, 0, 4095, -180, 180)
         # self.joy_y = lv.map(raw_y, 4095, 0, -180, 180)
-        self.joy_x = map_joystick(raw_x, 0, 4095, -180, 180, center, border)
-        self.joy_y = map_joystick(raw_y, 4095, 0, -180, 180, center, border)
+        self.joy_x = map_joystick(raw_x, 0, 4095, -180, 180, dead_center, dead_border)
+        self.joy_y = map_joystick(raw_y, 4095, 0, -180, 180, dead_center, dead_border)
 
     def refresh_wifi(self, timer):
         if self.wifi_label:
@@ -245,7 +245,7 @@ class RemoteControl(Activity):
                 msg = f"1:{self.joy_x},{self.joy_y}"
                 # print(f"--> Sending joystick: {msg}")
                 await ws.send_str(msg)
-                await asyncio.sleep(0.16)  # Send every 160ms
+                await asyncio.sleep(0.160)  # Send every 160ms
         except (asyncio.CancelledError, OSError):
             pass
         except Exception as e:
@@ -293,8 +293,7 @@ class RemoteControl(Activity):
                         joy_sender = asyncio.create_task(self.send_joystick_loop(ws))
                         receiver = asyncio.create_task(self.receive_loop(ws))
                         
-                        # await asyncio.gather(ping_sender, joy_sender, receiver)
-                        await asyncio.gather(receiver)
+                        await asyncio.gather(ping_sender, joy_sender, receiver)
 
             except asyncio.CancelledError:
                 print("WebSocket main task gracefully cancelled.")
